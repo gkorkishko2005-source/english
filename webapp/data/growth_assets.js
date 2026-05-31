de91aeb964df4a6484b393f4de924f2521265199
Gordey <oft@MacBook-Air-Gordey.local>
2026-06-01 00:01:45 +0300

    Add growth tree SVG assets

---
+++ webapp/data/growth_assets.js
@@ -0,0 +1,223 @@
+// webapp/data/growth_assets.js
+window.GROWTH_STAGES = [
+`<svg viewBox="0 0 200 220" width="200" height="220" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <path d="M30 200 Q100 196 170 200" stroke-opacity=".55"/>
+  <path d="M100 200 C100 194 100 187 100 181"/>
+  <path d="M99 189 C91 184 84 184 78 190 C86 193 94 193 99 189Z"/>
+  <path d="M101 189 C109 184 116 184 122 190 C114 193 106 193 101 189Z"/>
+  <path d="M100 181 C98 185 98 188 100 191 C102 188 102 185 100 181Z" stroke-opacity=".75"/>
+</svg>`,
+`<svg viewBox="0 0 200 220" width="200" height="220" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <path d="M30 200 Q100 196 170 200" stroke-opacity=".55"/>
+  <path d="M100 200 C99 186 100 168 100 140"/>
+  <path d="M99 181 C89 174 80 175 73 183 C83 187 92 187 99 181Z"/>
+  <path d="M101 181 C111 174 120 175 127 183 C117 187 108 187 101 181Z"/>
+  <path d="M99 160 C91 153 83 153 77 160 C86 164 94 164 99 160Z"/>
+  <path d="M101 160 C109 153 117 153 123 160 C114 164 106 164 101 160Z"/>
+  <path d="M100 140 C98 146 98 151 100 156 C102 151 102 146 100 140Z" stroke-opacity=".72"/>
+</svg>`,
+`<svg viewBox="0 0 200 220" width="200" height="220" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <path d="M30 200 Q100 196 170 200" stroke-opacity=".55"/>
+  <path d="M100 200 C98 178 100 153 100 132"/>
+  <path d="M100 132 C94 126 95 117 100 113 C105 117 106 126 100 132Z"/>
+  <path d="M99 179 C88 171 78 172 70 180 C82 186 92 185 99 179Z"/>
+  <path d="M101 179 C112 171 122 172 130 180 C118 186 108 185 101 179Z"/>
+  <path d="M99 151 C89 143 80 144 73 152 C84 157 93 157 99 151Z"/>
+  <path d="M101 151 C111 143 120 144 127 152 C116 157 107 157 101 151Z"/>
+  <path d="M100 134 C96 137 91 139 86 140" stroke-opacity=".7"/>
+  <path d="M100 134 C104 137 109 139 114 140" stroke-opacity=".7"/>
+</svg>`,
+`<svg viewBox="0 0 200 220" width="200" height="220" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <path d="M30 200 Q100 196 170 200" stroke-opacity=".55"/>
+  <path d="M100 200 C98 176 100 145 100 116"/>
+  <path d="M100 118 C91 111 88 100 93 91"/>
+  <path d="M100 118 C109 111 112 100 107 91"/>
+  <path d="M100 116 C100 106 100 98 100 88"/>
+  <path d="M99 177 C87 168 76 169 68 178 C81 184 92 184 99 177Z"/>
+  <path d="M101 177 C113 168 124 169 132 178 C119 184 108 184 101 177Z"/>
+  <path d="M99 146 C88 137 78 138 70 147 C83 153 93 152 99 146Z"/>
+  <path d="M101 146 C112 137 122 138 130 147 C117 153 107 152 101 146Z"/>
+  <path d="M100 88 C95 80 95 72 100 66 C105 72 105 80 100 88Z" stroke="var(--a)"/>
+  <path d="M100 88 C92 84 86 78 86 70 C94 70 100 78 100 88Z" stroke="var(--a)"/>
+  <path d="M100 88 C108 84 114 78 114 70 C106 70 100 78 100 88Z" stroke="var(--a)"/>
+  <path d="M100 88 C93 92 84 92 78 86 C86 82 95 83 100 88Z" stroke="var(--a)"/>
+  <path d="M100 88 C107 92 116 92 122 86 C114 82 105 83 100 88Z" stroke="var(--a)"/>
+  <circle cx="100" cy="88" r="2.4" fill="var(--a)" stroke="none"/>
+</svg>`,
+`<svg viewBox="0 0 200 220" width="200" height="220" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <path d="M30 200 Q100 196 170 200" stroke-opacity=".55"/>
+  <path d="M85 199 C91 188 96 172 99 145"/>
+  <path d="M115 199 C109 186 103 170 100 145"/>
+  <path d="M100 199 C100 178 100 158 100 132"/>
+  <path d="M100 164 C84 153 70 143 56 124"/>
+  <path d="M100 158 C118 148 132 137 146 118"/>
+  <path d="M100 142 C88 130 82 119 78 104"/>
+  <path d="M100 140 C113 128 121 116 126 101"/>
+  <path d="M51 133 C42 116 48 96 67 88 C76 72 100 70 112 84 C132 82 151 96 151 118 C163 132 157 154 140 161 C126 176 103 176 93 165 C75 172 56 161 51 133Z"/>
+  <path d="M72 122 C82 114 93 112 104 118"/>
+  <path d="M119 121 C131 118 140 122 147 132"/>
+  <path d="M68 152 C82 147 93 150 102 160"/>
+  <path d="M86 199 C77 195 66 195 58 199" stroke-opacity=".7"/>
+  <path d="M113 199 C123 195 134 195 143 199" stroke-opacity=".7"/>
+  <circle cx="82" cy="111" r="2.7" fill="var(--a)" stroke="none"/>
+  <circle cx="128" cy="130" r="2.7" fill="var(--a)" stroke="none"/>
+  <circle cx="103" cy="96" r="2.4" fill="var(--a)" stroke="none"/>
+</svg>`,
+`<svg viewBox="0 0 200 220" width="200" height="220" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <path d="M30 200 Q100 196 170 200" stroke-opacity=".55"/>
+  <path d="M93 200 C95 181 96 158 98 132"/>
+  <path d="M107 200 C105 181 103 158 102 132"/>
+  <path d="M100 198 C100 174 100 151 100 130"/>
+  <path d="M100 151 C85 143 75 131 67 115"/>
+  <path d="M101 148 C118 140 130 127 139 109"/>
+  <path d="M98 134 C91 125 87 114 86 102"/>
+  <path d="M102 134 C111 125 117 114 120 100"/>
+  <path d="M56 121 C52 91 74 70 101 70 C129 70 150 92 145 122 C153 141 139 161 117 163 C106 170 90 169 80 162 C61 160 49 141 56 121Z"/>
+  <path d="M68 119 C80 106 94 101 110 106"/>
+  <path d="M113 108 C126 107 136 114 143 126"/>
+  <path d="M79 152 C91 145 108 146 120 155"/>
+  <path d="M93 199 C83 194 72 194 61 200" stroke-opacity=".72"/>
+  <path d="M107 199 C119 194 132 194 145 200" stroke-opacity=".72"/>
+  <circle cx="83" cy="104" r="2.5" fill="var(--a)" stroke="none"/>
+  <circle cx="118" cy="91" r="2.3" fill="var(--a)" stroke="none"/>
+  <circle cx="132" cy="133" r="2.4" fill="var(--a)" stroke="none"/>
+  <circle cx="96" cy="147" r="2.2" fill="var(--a)" stroke="none"/>
+</svg>`,
+`<svg viewBox="0 0 200 220" width="200" height="220" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <path d="M30 200 Q100 196 170 200" stroke-opacity=".55"/>
+  <path d="M88 200 C92 174 94 145 96 118"/>
+  <path d="M112 200 C108 174 106 145 104 118"/>
+  <path d="M100 199 C100 170 100 143 100 116"/>
+  <path d="M100 149 C79 139 63 124 51 101"/>
+  <path d="M101 144 C122 135 138 119 150 96"/>
+  <path d="M97 124 C84 112 76 98 72 82"/>
+  <path d="M103 124 C118 112 128 98 134 80"/>
+  <path d="M35 110 C32 88 58 72 86 76 C104 60 136 64 150 82 C176 89 181 112 160 126 C149 141 121 143 101 131 C81 145 50 137 45 119 C39 118 36 115 35 110Z"/>
+  <path d="M59 107 C76 92 93 87 112 95"/>
+  <path d="M110 96 C130 94 147 104 158 121"/>
+  <path d="M70 140 C88 130 111 132 128 146"/>
+  <path d="M95 159 C89 171 90 184 92 199"/>
+  <path d="M105 158 C112 172 112 185 108 199"/>
+  <path d="M94 149 l-5 9"/>
+  <path d="M107 136 l6 8"/>
+  <path d="M90 199 C76 191 60 191 44 200"/>
+  <path d="M110 199 C127 191 144 191 160 200"/>
+  <path d="M99 199 C91 194 85 192 76 193" stroke-opacity=".72"/>
+  <path d="M101 199 C111 194 120 192 130 193" stroke-opacity=".72"/>
+  <circle cx="73" cy="105" r="2.8" fill="var(--a)" stroke="none"/>
+  <circle cx="113" cy="83" r="2.8" fill="var(--a)" stroke="none"/>
+  <circle cx="145" cy="117" r="2.8" fill="var(--a)" stroke="none"/>
+  <circle cx="91" cy="141" r="2.6" fill="var(--a)" stroke="none"/>
+  <circle cx="126" cy="143" r="2.6" fill="var(--a)" stroke="none"/>
+</svg>`,
+`<svg viewBox="0 0 200 220" width="200" height="220" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <path d="M30 200 Q100 196 170 200" stroke-opacity=".55"/>
+  <path d="M84 200 C90 170 93 137 94 103"/>
+  <path d="M116 200 C110 170 107 137 106 103"/>
+  <path d="M100 199 C100 165 100 131 100 96"/>
+  <path d="M100 142 C75 131 55 112 38 82"/>
+  <path d="M101 136 C128 125 149 105 164 76"/>
+  <path d="M96 108 C78 94 67 76 62 56"/>
+  <path d="M104 107 C123 91 136 72 142 52"/>
+  <path d="M48 127 C25 112 22 75 48 55 C54 26 93 22 110 43 C134 25 172 40 174 74 C194 91 184 126 160 137 C152 169 112 176 98 155 C78 178 37 159 48 127Z"/>
+  <path d="M43 88 C62 70 86 64 109 73"/>
+  <path d="M109 73 C132 68 154 80 170 100"/>
+  <path d="M58 130 C81 115 112 117 136 137"/>
+  <path d="M73 58 C88 57 99 63 109 73"/>
+  <path d="M126 58 C142 60 155 70 164 84"/>
+  <path d="M94 123 l-7 10"/>
+  <path d="M106 116 l7 9"/>
+  <path d="M95 149 l-8 12"/>
+  <path d="M108 151 l9 12"/>
+  <path d="M86 200 C70 187 51 188 33 200"/>
+  <path d="M114 200 C132 187 153 188 171 200"/>
+  <path d="M99 199 C85 190 75 187 60 190"/>
+  <path d="M101 199 C117 190 130 187 145 190"/>
+  <path d="M94 198 C90 187 87 178 80 170" stroke-opacity=".7"/>
+  <path d="M106 198 C113 186 119 178 128 171" stroke-opacity=".7"/>
+  <circle cx="62" cy="90" r="2.8" fill="var(--a)" stroke="none"/>
+  <circle cx="91" cy="61" r="2.8" fill="var(--a)" stroke="none"/>
+  <circle cx="129" cy="73" r="2.8" fill="var(--a)" stroke="none"/>
+  <circle cx="152" cy="111" r="2.8" fill="var(--a)" stroke="none"/>
+  <circle cx="82" cy="136" r="2.7" fill="var(--a)" stroke="none"/>
+  <circle cx="118" cy="145" r="2.7" fill="var(--a)" stroke="none"/>
+  <path d="M54 34 h7 M57.5 30.5 v7" stroke="var(--a)" stroke-width="1.4"/>
+  <path d="M100 22 h7 M103.5 18.5 v7" stroke="var(--a)" stroke-width="1.4"/>
+  <path d="M148 33 h7 M151.5 29.5 v7" stroke="var(--a)" stroke-width="1.4"/>
+  <path d="M124 39 h5 M126.5 36.5 v5" stroke="var(--a)" stroke-width="1.4"/>
+</svg>`
+];
+
+window.WEATHER_OVERLAYS = {
+  fresh: `<svg viewBox="0 0 200 220" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" fill="none" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <g stroke="var(--a)" stroke-width="1.6" opacity=".85" transform="translate(150 42)">
+    <g>
+      <animateTransform attributeName="transform" type="rotate" from="0" to="360" dur="9s" repeatCount="indefinite"/>
+      <path d="M0 -20 V-27 M0 20 V27 M-20 0 H-27 M20 0 H27 M-14 -14 L-19 -19 M14 -14 L19 -19 M-14 14 L-19 19 M14 14 L19 19"/>
+    </g>
+  </g>
+  <g stroke="var(--a)" stroke-width="1.6" opacity=".78">
+    <animate attributeName="opacity" values=".62;.9;.62" dur="5s" repeatCount="indefinite"/>
+    <circle cx="150" cy="42" r="10"/>
+  </g>
+</svg>`,
+  breezy: `<svg viewBox="0 0 200 220" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <g opacity="0">
+    <animateTransform attributeName="transform" type="translate" from="-24 26" to="214 80" dur="8s" repeatCount="indefinite"/>
+    <animate attributeName="opacity" values="0;.62;.62;0" keyTimes="0;.18;.82;1" dur="8s" repeatCount="indefinite"/>
+    <path d="M0 0 C3 -2 7 -2 10 0 C7 2 3 2 0 0Z"/>
+  </g>
+  <g opacity="0">
+    <animateTransform attributeName="transform" type="translate" from="-18 62" to="210 112" dur="6.8s" begin="1.5s" repeatCount="indefinite"/>
+    <animate attributeName="opacity" values="0;.48;.48;0" keyTimes="0;.16;.82;1" dur="6.8s" begin="1.5s" repeatCount="indefinite"/>
+    <path d="M0 0 C3 -2 7 -2 10 0 C7 2 3 2 0 0Z"/>
+  </g>
+  <g opacity="0">
+    <animateTransform attributeName="transform" type="translate" from="-30 42" to="204 42" dur="9s" begin="3s" repeatCount="indefinite"/>
+    <animate attributeName="opacity" values="0;.36;.36;0" keyTimes="0;.18;.78;1" dur="9s" begin="3s" repeatCount="indefinite"/>
+    <path d="M0 0 C3 -2 7 -2 10 0 C7 2 3 2 0 0Z"/>
+  </g>
+</svg>`,
+  windy: `<svg viewBox="0 0 200 220" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <g opacity=".36">
+    <path d="M18 45 C27 35 44 36 50 48 C59 46 70 52 72 62 C60 63 42 63 25 62"/>
+  </g>
+  <g opacity=".22">
+    <animate attributeName="opacity" values=".12;.48;.12" dur="1.5s" repeatCount="indefinite"/>
+    <path d="M42 78 L106 70 M26 96 L138 84 M72 114 L160 104"/>
+  </g>
+  <g opacity="0">
+    <animateTransform attributeName="transform" type="translate" from="-30 58" to="230 116" dur="3.5s" repeatCount="indefinite"/>
+    <animate attributeName="opacity" values="0;.68;.68;0" keyTimes="0;.12;.84;1" dur="3.5s" repeatCount="indefinite"/>
+    <path d="M0 0 C4 -3 10 -2 14 1 C9 4 4 3 0 0Z"/>
+  </g>
+  <g opacity="0">
+    <animateTransform attributeName="transform" type="translate" from="-45 94" to="224 42" dur="3.1s" begin=".8s" repeatCount="indefinite"/>
+    <animate attributeName="opacity" values="0;.58;.58;0" keyTimes="0;.12;.82;1" dur="3.1s" begin=".8s" repeatCount="indefinite"/>
+    <path d="M0 0 C4 -3 10 -2 14 1 C9 4 4 3 0 0Z"/>
+  </g>
+  <g opacity="0">
+    <animateTransform attributeName="transform" type="translate" from="-24 36" to="220 86" dur="3.8s" begin="1.6s" repeatCount="indefinite"/>
+    <animate attributeName="opacity" values="0;.45;.45;0" keyTimes="0;.12;.82;1" dur="3.8s" begin="1.6s" repeatCount="indefinite"/>
+    <path d="M0 0 C4 -3 10 -2 14 1 C9 4 4 3 0 0Z"/>
+  </g>
+</svg>`,
+  rainy: `<svg viewBox="0 0 200 220" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+  <g opacity=".42">
+    <animateTransform attributeName="transform" type="translate" values="-4 0;4 0;-4 0" dur="14s" repeatCount="indefinite"/>
+    <path d="M42 51 C54 32 84 32 96 53 C110 47 133 55 139 73 C121 77 89 77 55 76 C38 76 30 64 42 51Z"/>
+  </g>
+  <g opacity=".5">
+    <animateTransform attributeName="transform" type="translate" from="0 -48" to="-26 190" dur="1.15s" repeatCount="indefinite"/>
+    <path d="M58 85 l-8 16 M82 72 l-8 16 M111 84 l-8 16 M135 70 l-8 16"/>
+  </g>
+  <g opacity=".42">
+    <animateTransform attributeName="transform" type="translate" from="0 -68" to="-26 170" dur="1.35s" begin=".35s" repeatCount="indefinite"/>
+    <path d="M48 80 l-8 16 M72 94 l-8 16 M102 70 l-8 16 M126 92 l-8 16"/>
+  </g>
+  <g opacity=".36">
+    <animateTransform attributeName="transform" type="translate" from="0 -56" to="-26 182" dur="1.25s" begin=".7s" repeatCount="indefinite"/>
+    <path d="M67 76 l-8 16 M94 90 l-8 16 M120 76 l-8 16 M146 88 l-8 16"/>
+  </g>
+</svg>`
+};
